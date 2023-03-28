acdcc4eaaacb2407f968fbbea89282262e805b1d
Everton de Oliveira <gurupiman@gmail.com>
2023-03-28 03:01:17 -0300

    first commit

---
+++ iac/emr-code/pyspark/job_spark_from_tf.py
@@ -0,0 +1,12 @@
+import pyspark
+
+
+enem = (
+    spark
+    .read
+    .format("csv")
+    .option("header", True)
+    .option("inferSchema", True)
+    .option("delimiter", ";")
+    .load("s3://datalake-everton-01/data/")
+)
---
+++ iac/variables.tf
@@ -0,0 +1,15 @@
+variable "base_bucket_name" {
+  default = "datalake-xpe-tf"
+}
+
+variable "ambiente" {
+  default = "producao"
+}
+
+variable "numero_conta" {
+  default = "899015669891"
+}
+
+variable "regiao" {
+  default = "us-east-2"
+}
